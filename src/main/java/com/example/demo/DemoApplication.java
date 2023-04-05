9d6c4cf79202c23512078c4e5739cb592b253011
T14s <T14s@DESKTOP-4KA89TO>
2023-04-04 19:33:07 -0500

    Version 3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "!Hola Mundo Kairos desde Github!";
+        return "!Hola Mundo Kairos desde Github version 2!";
     }
 
     public static void main(String[] args) {
